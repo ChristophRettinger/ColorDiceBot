4499995af3caf798d669279b7d3f5defbd479a42
Christoph Rettinger <christoph.rettinger@TRITECH.AT>
2016-06-16 16:41:11 +0200

    Added help

--- bot.js
+++ bot.js
@@ -5,9 +5,16 @@ var Bot = require('node-telegram-bot-api'),
 
 console.log('bot server started...');
 
-bot.onText(/^\/say_hello (.+)$/, function (msg, match) {
+bot.onText(/^\/hello (.+)$/, function (msg, match) {
   var name = match[1];
   bot.sendMessage(msg.chat.id, 'Hello ' + name + '!').then(function () {
+    // reply sent!
+  });
+});
+
+bot.onText(/^\/help$/, function (msg) {
+  var name = match[1];
+  bot.sendMessage(msg.chat.id, '/hello [Name]').then(function () {
     // reply sent!
   });
 });
