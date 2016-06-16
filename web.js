77901ae34c9145c47a79ad105268155bcf49356b
Christoph Rettinger <christoph.rettinger@TRITECH.AT>
2016-06-16 15:13:25 +0200

    package.json added for openshift. web.js server parameter changed

--- web.js
+++ web.js
@@ -7,7 +7,10 @@ app.get('/', function (req, res) {
   res.json({ version: packageInfo.version });
 });
 
-var server = app.listen(process.env.PORT, function () {
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+var server = app.listen(server_port, server_ip_address, function () {
   var host = server.address().address;
   var port = server.address().port;
 
